d20de467050a4ec42addbaf190484ad27ca7ff5b
DeevsDeevs <deevsgt15@gmail.com>
2025-04-26 06:59:51 +0200

    fix: stylua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,70 +1,80 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-            ensure_installed = {
-                "lua_ls",
-                -- "basedpyright",
-                "ruff",
-                "clangd",
-                "stylua",
-            },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = {
+				"lua_ls",
+				-- "basedpyright",
+				"ruff",
+				"clangd",
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"stylua", -- Lua formatter
+			},
+			auto_update = true,
+			run_on_start = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            -- lspconfig.basedpyright.setup({
-            -- capabilities = capabilities,
-            -- settings = {
-            --    basedpyright = {
-            --        disableOrganizeImports = true,
-            --   },
-            -- python = {
-            --       analysys = {
-            --        ignore = { "*" },
-            --  },
-            --},
-            --},
-            --})
-            lspconfig.ruff.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.basedpyright.setup({
+			-- capabilities = capabilities,
+			-- settings = {
+			--    basedpyright = {
+			--        disableOrganizeImports = true,
+			--   },
+			-- python = {
+			--       analysys = {
+			--        ignore = { "*" },
+			--  },
+			--},
+			--},
+			--})
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-            vim.diagnostic.config({
-                virtual_text = {
-                    prefix = "●", -- or "▶", ">>", etc.
-                    spacing = 4,
-                },
-                signs = true,
-                underline = true,
-                update_in_insert = false,
-                severity_sort = true,
-            })
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●", -- or "▶", ">>", etc.
+					spacing = 4,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
