a5ddb1208f06da305e1886e4801819fdd05ab792
DeevsDeevs <deevsgt15@gmail.com>
2025-10-05 14:48:01 +0300

    fix: missing yank deps for ubuntu

--- setup.sh
+++ setup.sh
@@ -53,6 +53,7 @@ install_if_missing vivid vivid
 install_if_missing zoxide zoxide
 install_if_missing tmux tmux
 install_if_missing git git
+install_if_missing xclip xclip
 
 # Install Starship
 if ! command -v starship &> /dev/null; then
